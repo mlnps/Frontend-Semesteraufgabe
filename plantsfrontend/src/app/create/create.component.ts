38926e34c2de0b804348be17472cecd4890eaa53
milena <pustet@posteo.de>
2021-09-28 19:24:55 +0200

    add possibility to upload image from computer and code cleanup

--- plantsfrontend/src/app/create/create.component.ts
+++ plantsfrontend/src/app/create/create.component.ts
@@ -30,8 +30,6 @@ export class CreateComponent implements OnInit {
   ngOnInit(): void {
     this.formGroup = this.fb.group({
       inp_title: ['', Validators.required],
-      inp_location: ['', Validators.required],
-      // inp_image: ['', Validators.required],
     });
   }
 
@@ -42,15 +40,6 @@ export class CreateComponent implements OnInit {
   get inp_location(): FormControl {
     return this.formGroup.get('inp_location') as FormControl;
   }
-
-
-
-
-
-
-/*  get inp_image(): FormControl { // TODO: brauchen wir nicht?
-    return this.formGroup.get('inp_image') as FormControl;
-  }*/
 
   initializeMedia() {
     const n = <any>navigator;
@@ -152,45 +141,28 @@ export class CreateComponent implements OnInit {
     }
   }
 
-/*  openCreatePostModal() { // TODO: wird nirgends aufgerufen.. brauchen wir das überhaupt?
-    createPostArea!.style.transform = 'translateY(0)';
-    this.initializeMedia();
-    this.initializeLocation();
-  }*/
-
-/*  closeCreatePostModal() { //  TODO: wird nirgends aufgerufen.. brauchen wir das überhaupt?
-    createPostArea.style.transform = 'translateY(100vH)';
-    // imagePickerArea.style.display = 'none';
-    // videoPlayer.style.display = 'none';
-    // canvasElement.style.display = 'none';
-    //locationButton.style.display = 'inline';
-    //locationLoader.style.display = 'none';
-  }*/
 
   onSubmit(): void {
-    // if (!this.inp_title || !this.inp_title.value || !this.inp_location || !this.inp_location.value || !this.imageBase64) {
-    //   alert('Bitte die Form komplett ausfüllen.');
-    //   return;
-    // }
+    const locationInput = <HTMLInputElement>document.querySelector('#location');
 
     const plant = {
       id: null,
       title: this.inp_title.value,
-      location: this.inp_location.value,//this.inp_location.value,
+      location: locationInput.value,
       image: this.imageBase64,
     };
     console.log('plant : ', plant);
     this.bs.addPlant(plant).then((res) => {
       console.log(res);
-      
+
     });
-    
+
     this.router.navigateByUrl('/');
-    
+
 
   }
 
-/*  uploadFileEvt(imgFile: any): void { //TODO: wird nirgends aufgerufen.. brauchen wir das überhaupt?
+  uploadFileEvt(imgFile: any): void {
     console.log('upload', imgFile.target.files);
     if (imgFile.target.files && imgFile.target.files[0]) {
       const reader = new FileReader();
@@ -208,5 +180,5 @@ export class CreateComponent implements OnInit {
       };
       reader.readAsDataURL(imgFile.target.files[0]);
     }
-  }*/
+  }
 }
